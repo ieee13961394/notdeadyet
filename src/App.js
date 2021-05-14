e4f30c2a242e029241e404596e5b721b596822b5
ieee13961394 <rmo@remcon.me>
2021-05-14 11:01:35 +0100

    Added Image Upload Function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { API } from 'aws-amplify';
+import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+
 
 const initialFormState = { name: '', description: '' }
 
@@ -15,17 +16,37 @@ function App() {
     fetchNotes();
   }, []);
 
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    setNotes(apiData.data.listNotes.items);
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
   }
 
-  async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
+  async function fetchNotes() {
+  const apiData = await API.graphql({ query: listNotes });
+  const notesFromAPI = apiData.data.listNotes.items;
+  await Promise.all(notesFromAPI.map(async note => {
+    if (note.image) {
+      const image = await Storage.get(note.image);
+      note.image = image;
+    }
+    return note;
+  }))
+  setNotes(apiData.data.listNotes.items);
+}
+
+async function createNote() {
+  if (!formData.name || !formData.description) return;
+  await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+  if (formData.image) {
+    const image = await Storage.get(formData.image);
+    formData.image = image;
   }
+  setNotes([ ...notes, formData ]);
+  setFormData(initialFormState);
+}
 
   async function deleteNote({ id }) {
     const newNotesArray = notes.filter(note => note.id !== id);
